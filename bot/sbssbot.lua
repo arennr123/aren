89bcafc0308475b85494af6665d2d7d7e4822947
arennr123 <arennr123@gmail.com>
2016-04-12 16:18:52 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,18 +239,18 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {144658773,114934518,94746365},--Sudo users
+    sudo_users = {153028603},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
     An Advanced Anti Spam Bot Forked On TeleSeed
     
     Develpoed By:
-    @sina7sk
+    @ANAREN
     Manager:
-    @yellowhat
+    @ANAREN
     Founder:
-    @amirho3in
+    @aNAREN
     
     Special Thank To:
     Mehr Pouya
