fe4d0d34ac57bcbbdc25fc2184153871c33e879b
arennr123 <arennr123@gmail.com>
2016-04-13 19:33:01 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {153028603},--Sudo users
+    sudo_users = {105416839},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
